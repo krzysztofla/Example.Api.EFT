504411b301df4a6e1483e6d7c7a021e5a81e9c98
krzysztofla <krzysztof.lach@icloud.com>
2022-01-03 15:21:42 +0100

    Setting project backbone and structure

---
+++ src/Example.Core.EFT/Entities/Item.cs
@@ -0,0 +1,24 @@
+﻿using Example.Core.EFT.Value_Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Core.EFT.Entities
+{
+    internal class Item
+    {
+        public Guid id { get; private set; }    
+        private readonly Price _price;
+
+        private readonly Description _description;
+
+        public Item(Guid id, Price price, Description description)
+        {
+            id = id;
+            _price = price;
+            _description = description;
+        }
+    }
+}
